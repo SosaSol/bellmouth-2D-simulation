dd7a1e9cfd6daac46ab7915aed628b73b9f6042f
SosaSol <solim.rovera2001@gmail.com>
2025-05-14 16:50:05 +0200

    Remove obsolete geometry tests and add new tests for mesh_advanced_no_bellmouth

--- tests/tests_params.py
+++
@@ -1,8 +0,0 @@
-from mesh import parse_args
-import sys
-
-def test_default_args(monkeypatch):
-    monkeypatch.setattr(sys, 'argv', ['mesh.py'])
-    args = parse_args()
-    assert args.Mw == 12
-    assert args.Kx == 0.3
